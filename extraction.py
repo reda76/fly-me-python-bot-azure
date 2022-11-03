7fad7be513bfc6259a9390f8a76c666cec738936
redad76 <derraz.mohamedreda@gmail.com>
2022-11-03 17:26:02 +0100

    Add extraction of message and send of the result of extraction

---
+++ extraction.py
@@ -0,0 +1,115 @@
+from dateutil.parser import parse
+from price_parser import Price
+
+import numpy as np
+import datetime
+import re
+
+
+def result_to_json(result):
+    luis_result = str(result)
+
+    luis_result = re.sub(
+            r">", 
+            ">'", luis_result
+        )
+
+    luis_result = re.sub(
+            r"<", 
+            "'<", luis_result
+        )
+    luis_result = eval(luis_result)
+
+    return luis_result
+
+# Fonction de formatage des budgets et dates
+def parse_price(price):
+    try:
+        price = Price.fromstring(price, decimal_separator=".")
+        return price.amount_float
+    except:
+        return "None"
+
+def parse_date(date):
+    try:
+        return parse(date, fuzzy_with_tokens=True)[0]
+    except:
+        return "None"
+
+def extract(result):
+    dict_information = {
+            "text": "None",
+            "or_city": "None",
+            "dst_city": "None",
+            "str_date": "None",
+            "end_date": "None",
+            "budget": "None",
+            "datetimeV2": "None",
+            "geography": "None",
+            "money": "None"
+            }
+    
+    dict_information["text"] = result["text"]
+    
+    list_information_fly = ["or_city", "dst_city", "str_date", "end_date", "budget"]
+    
+    dict_fly = result["entities"]["FlyOrder"][0]['Fly'][0]
+    
+    for inf_fly in list_information_fly:
+        if inf_fly == "budget":
+            try:
+                dict_information[inf_fly] = parse_price(dict_fly[inf_fly][0])
+            except:
+                dict_information[inf_fly] = "None" 
+        elif inf_fly == "str_date":
+            try:
+                dict_information[inf_fly] = parse_date(dict_fly[inf_fly][0]).date()
+            except:
+                dict_information[inf_fly] = "None"
+        elif inf_fly == "end_date":
+            try:
+                dict_information[inf_fly] = parse_date(dict_fly[inf_fly][0]).date()
+            except:
+                dict_information[inf_fly] = "None"
+        else:
+            try:
+                dict_information[inf_fly] = dict_fly[inf_fly][0]
+            except:
+                dict_information[inf_fly] = "None"
+    
+    liste_geo = []
+    # Il peut y avoir plusieurs informations stockées dans la géographie
+    # nous avons donc mis une condition si deux étaient trouvées
+    if len(result["entities"]["geographyV2"]) > 1:
+        liste_geo.append(result["entities"]["geographyV2"][0]["location"])
+        liste_geo.append(result["entities"]["geographyV2"][1]["location"])
+    else:
+        liste_geo = result["entities"]["geographyV2"][0]["location"]
+    dict_information["geography"] = liste_geo  
+        
+    liste_datetime = []
+    date_time_brute = result["entities"]["datetime"][0]["timex"][0]
+    
+    # Le tuple est dans un string mais le contenu du tuple ne l'est pas
+    # nous de devons donc ajouter des string à chaque élément
+    date_time_brute = re.sub(r"\(", "('", date_time_brute)
+    date_time_brute = re.sub(r"\)", "')", date_time_brute)
+    date_time_brute = re.sub(r",", "','", date_time_brute)
+    try:
+        date_time_brute = eval(date_time_brute)
+    except:
+        date_time_brute = str(date_time_brute)
+    if type(date_time_brute) is tuple:
+        for i in date_time_brute:
+            if len(date_time_brute) != 0:
+                if i[0] != "P":
+                    liste_datetime.append(parse_date(i).date())
+    else:
+        if len(date_time_brute) != 0:
+            if date_time_brute[0] != "P":
+                liste_datetime.append(parse_date(date_time_brute).date())
+    dict_information["datetimeV2"] = liste_datetime
+    
+    dict_information["money"] = float(result["entities"]["money"][0]['number'])
+    
+    return dict_information
--- luisbot.py
+++ luisbot.py
@@ -1,7 +1,11 @@
+import re
+import json
+
 from botbuilder.core import TurnContext,ActivityHandler
-from botbuilder.ai.luis import LuisApplication,LuisPredictionOptions,LuisRecognizer
+from botbuilder.ai.luis import LuisApplication,LuisRecognizer, LuisRecognizerOptionsV3
+
 from config import DefaultConfig
-import os
+from extraction import extract, result_to_json
 
 CONFIG = DefaultConfig()
 
@@ -16,14 +20,28 @@ endpointPrediction_url =CONFIG.ENDPOINT_PREDICTION_URL
 class LuisBot(ActivityHandler):
     def __init__(self):
         luis_app = LuisApplication(app_id, appPrediction_Key, endpointPrediction_url)
-        luis_option = LuisPredictionOptions(include_all_intents=True,include_instance_data=True)
+        luis_option = LuisRecognizerOptionsV3(include_all_intents=False,include_instance_data=False)
         self.LuisReg = LuisRecognizer(luis_app,luis_option,True)
 
 
     async def on_turn(self, turn_context:TurnContext):
         luis_result = await self.LuisReg.recognize(turn_context)
         intent = LuisRecognizer.top_intent(luis_result)
-        await turn_context.send_activity(luis_result.text)
-        print(luis_result.text)
-        print()
+
+        # Nous avons besoin de modifier une valeur, le dictionnaire return dans la valeur de la clé "Booking" ceci :
+        # <botbuilder.core.intent_score.IntentScore object at 0x000001C516A00910>, et renvoie donc une erreur de syntaxe
+        # Nous devons donc rentre le dictionnaire en string, et ajouté des strings à cette partie pour avoir un dictionnaire normal
+        luis_result = result_to_json(luis_result)
         print(luis_result)
+        dict_extract = extract(luis_result)
+
+        origine = dict_extract["or_city"]
+        destination = dict_extract["dst_city"]
+        start_date = dict_extract["str_date"]
+        end_date = dict_extract["end_date"]
+        budget = dict_extract["budget"]
+        datetimeV = dict_extract["datetimeV2"]
+        geographyV = dict_extract["geography"]
+        money = dict_extract["money"]
+
+        await turn_context.send_activity(f"Do you want to go to {destination} from {origine} on {start_date} to {end_date} for a budget of {budget} ?")
